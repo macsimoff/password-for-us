2c1aef115c8dff27bdcc870081f53236d37c3560
Oleg Maksimov <oleg.macsimoff@gmail.com>
2025-11-06 02:40:20 +0500

    test: add unit tests for AES encryption and PBKDF2 hashing services

---
+++ TestsForPasswordForUs/Encryption/AesEncryptionServiceTest.cs
@@ -0,0 +1,122 @@
+﻿using System.Security.Cryptography;
+using Encryption.Aes;
+using PasswordForUs.Abstractions;
+using PasswordForUs.Abstractions.Models;
+
+namespace TestsForPasswordForUs.Encryption;
+
+[TestFixture]
+public class AesEncryptionServiceTest
+{
+    private static AesEncryptionService CreateService(int iterations = 10) => new(new TestPasswordHashing(), iterations);
+
+    private static byte[] NewKey(int len = 32)
+    {
+        var key = new byte[len];
+        RandomNumberGenerator.Fill(key);
+        return key;
+    }
+
+    [Test]
+    public void EncryptDecrypt_Roundtrip_Ok()
+    {
+        var service = CreateService();
+        var masterKey = NewKey();
+        var node = new NodeData
+        {
+            Login = "user1",
+            Password = "p@ss",
+            Data = new Dictionary<string, string> { ["note"] = "hello" }
+        };
+
+        var enc = service.Encrypt(masterKey, node);
+        Assert.That(enc.Bytes, Is.Not.Null.And.Not.Empty);
+
+        var dec = service.Decrypt(masterKey, enc);
+
+        Assert.That(dec.Login, Is.EqualTo(node.Login));
+        Assert.That(dec.Password, Is.EqualTo(node.Password));
+        Assert.That(dec.Data, Is.EquivalentTo(node.Data));
+    }
+
+    [Test]
+    public void Encrypt_SameInput_ProducesDifferentCiphertext()
+    {
+        var service = CreateService();
+        var masterKey = NewKey();
+        var node = new NodeData
+        {
+            Login = "user",
+            Password = "pwd",
+            Data = new Dictionary<string, string> { ["k"] = "v" }
+        };
+
+        var enc1 = service.Encrypt(masterKey, node);
+        var enc2 = service.Encrypt(masterKey, node);
+
+        Assert.That(enc1.Bytes, Is.Not.EqualTo(enc2.Bytes));
+    }
+
+    [Test]
+    public void Encrypt_EmptyMasterKey_Throws()
+    {
+        var service = CreateService();
+        var node = new NodeData { Login = "u", Password = "p", Data = new Dictionary<string, string>() };
+
+        Assert.Throws<ArgumentException>(() => service.Encrypt(Array.Empty<byte>(), node));
+        Assert.Throws<ArgumentException>(() => service.Encrypt(null!, node));
+    }
+
+    [Test]
+    public void Decrypt_EmptyMasterKey_Throws()
+    {
+        var service = CreateService();
+        var enc = new EncryptedData { Bytes = new byte[48] };
+
+        Assert.Throws<ArgumentException>(() => service.Decrypt(Array.Empty<byte>(), enc));
+        Assert.Throws<ArgumentException>(() => service.Decrypt(null!, enc));
+    }
+
+    [Test]
+    public void Decrypt_InvalidFormat_Throws()
+    {
+        var service = CreateService();
+        var masterKey = NewKey();
+        var enc = new EncryptedData { Bytes = new byte[10] };
+
+        Assert.Throws<CryptographicException>(() => service.Decrypt(masterKey, enc));
+    }
+
+    [Test]
+    public void Decrypt_WrongMasterKey_Throws()
+    {
+        var service = CreateService();
+        var key1 = NewKey();
+        var key2 = NewKey();
+        var node = new NodeData
+        {
+            Login = "u",
+            Password = "p",
+            Data = new Dictionary<string, string> { ["a"] = "b" }
+        };
+
+        var enc = service.Encrypt(key1, node);
+
+        Assert.Throws<AuthenticationTagMismatchException>(() => service.Decrypt(key2, enc));
+    }
+
+    [Test]
+    public void Ctor_NonPositiveIterations_Throws()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new AesEncryptionService(new TestPasswordHashing(), 0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new AesEncryptionService(new TestPasswordHashing(), -1));
+    }
+
+    private sealed class TestPasswordHashing : IPasswordHashing
+    {
+        public byte[] GetHash_256(byte[] password, byte[] salt, int iterations)
+        {
+            return Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, HashAlgorithmName.SHA256, 32);
+        }
+    }
+}
---
+++ TestsForPasswordForUs/PasswordHashing/Pbkdf2HashingTest.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using PasswordHashing;
+
+namespace TestsForPasswordForUs.PasswordHashing
+{
+    [TestFixture]
+    public class Pbkdf2HashingTests
+    {
+        private readonly Pbkdf2Hashing _sut = new();
+
+        [Test]
+        public void GetHash_256_Returns32Bytes()
+        {
+            var master = Encoding.UTF8.GetBytes("password");
+            var salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var hash = _sut.GetHash_256(master, salt, 10000);
+
+            Assert.AreEqual(32, hash.Length);
+        }
+
+        [Test]
+        public void GetHash_256_IsDeterministic_ForSameInputs()
+        {
+            var master = Encoding.UTF8.GetBytes("password");
+            var salt = new byte[] { 10, 20, 30, 40 };
+            var iterations = 15000;
+
+            var h1 = _sut.GetHash_256(master, salt, iterations);
+            var h2 = _sut.GetHash_256(master, salt, iterations);
+
+            CollectionAssert.AreEqual(h1, h2);
+        }
+
+        [Test]
+        public void GetHash_256_DifferentSalt_ProducesDifferentHash()
+        {
+            var master = Encoding.UTF8.GetBytes("password");
+            var salt1 = new byte[] { 1, 2, 3, 4 };
+            var salt2 = new byte[] { 4, 3, 2, 1 };
+
+            var h1 = _sut.GetHash_256(master, salt1, 10000);
+            var h2 = _sut.GetHash_256(master, salt2, 10000);
+
+            CollectionAssert.AreNotEqual(h1, h2);
+        }
+
+        [Test]
+        public void GetHash_256_DifferentMasterKey_ProducesDifferentHash()
+        {
+            var master1 = Encoding.UTF8.GetBytes("password1");
+            var master2 = Encoding.UTF8.GetBytes("password2");
+            var salt = new byte[] { 1, 2, 3, 4 };
+
+            var h1 = _sut.GetHash_256(master1, salt, 10000);
+            var h2 = _sut.GetHash_256(master2, salt, 10000);
+
+            CollectionAssert.AreNotEqual(h1, h2);
+        }
+
+        [Test]
+        public void GetHash_256_DifferentIterations_ProducesDifferentHash()
+        {
+            var master = Encoding.UTF8.GetBytes("password");
+            var salt = new byte[] { 1, 2, 3, 4 };
+
+            var h1 = _sut.GetHash_256(master, salt, 1000);
+            var h2 = _sut.GetHash_256(master, salt, 2000);
+
+            CollectionAssert.AreNotEqual(h1, h2);
+        }
+
+        [Test]
+        public void GetHash_256_Throws_OnNullMasterKey()
+        {
+            var salt = new byte[] { 1, 2, 3, 4 };
+            Assert.Throws<ArgumentNullException>(() => _sut.GetHash_256(null!, salt, 1000));
+        }
+
+        [Test]
+        public void GetHash_256_Throws_OnNullSalt()
+        {
+            var master = Encoding.UTF8.GetBytes("password");
+            Assert.Throws<ArgumentNullException>(() => _sut.GetHash_256(master, null!, 1000));
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void GetHash_256_Throws_OnInvalidIterations(int iterations)
+        {
+            var master = Encoding.UTF8.GetBytes("password");
+            var salt = new byte[] { 1, 2, 3, 4 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => _sut.GetHash_256(master, salt, iterations));
+        }
+    }
+}
